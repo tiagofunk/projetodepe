e8ef899dfd6a4d7163e53a896cccc1965e199367
Tiago Funk <tiagoff.tf@gmail.com>
2022-10-28 20:19:01 -0300

    Mensagem inicial do site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import logo from './img/depe.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          No futuro o site do projeto de pé estara aqui.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
