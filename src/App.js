38f0139443e0ab2b4cc12c0f17cc7882ce0b2e91
Raphael De Souza Ramalho <rsramalhovf@gmail.com>
2022-12-17 11:40:51 -0300

    preparing enviroment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} />
+        <img src={logo} alt=""/>
         <p>
           No futuro o site do projeto de pé estara aqui.
         </p>
