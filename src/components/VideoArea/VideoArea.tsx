076f0b6a575f4eee8ac65f6d1f1e840769f396a8
Tiago Funk <tiagoff.tf@gmail.com>
2023-01-09 23:05:58 -0300

    Alteração da tag que adiciona vídeo do youtube

--- src/components/VideoArea/VideoArea.tsx
+++ src/components/VideoArea/VideoArea.tsx
@@ -1,25 +1,14 @@
 import { Container } from "./VideoArea.style";
 
-import YouTube, { YouTubeProps } from "react-youtube";
-
 export const VideoArea = () => {
-  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
-  };
-
-  const opts: YouTubeProps["opts"] = {
-    height: "566rem",
-    width: "100%",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
-  };
-
   return (
     <Container>
-      <YouTube videoId="2grAUNpnG8I" opts={opts} onReady={onPlayerReady} />
+      <iframe
+        title="videoProjetodePe"
+        width="100%"
+        height="566rem"
+        src="https://www.youtube.com/embed/2grAUNpnG8I"
+      ></iframe>
     </Container>
   );
 };
