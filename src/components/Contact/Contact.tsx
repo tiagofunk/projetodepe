dbeb2a7b276c21c317626a247274fd96afb314e5
Tiago Funk <tiagoff.tf@gmail.com>
2023-01-18 19:34:17 -0300

    Limpeza do código

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -16,10 +16,16 @@ const { Option } = MySelect;
 export const Contact = () => {
   const title = "Complete o formulário e entre em contato com a gente";
   const nameTitle = "Nome";
+  const nameFailMessage = "Nome é necessário";
   const emailTitle = "E-mail";
+  const emailFailMessage = "E-mail é necessário";
   const subjectTitle = "Assunto";
+  const SubjectFailMessage = "Assunto é necessário";
   const phoneTitle = "Telefone/Celular";
+  const phoneFailMessage = "Telefone é necessário";
   const messageTitle = "Mensagem";
+  const messageFailMessage = "Mensagem é necessário";
+  const messageRows = 4;
   const buttonText = "Enviar Mensagem";
 
   const options = [
@@ -37,60 +43,103 @@ export const Contact = () => {
     console.log("foi", values);
   };
 
+  function getFieldName() {
+    return (
+      <>
+        <Text>{nameTitle}</Text>
+        <Form.Item
+          name={nameTitle}
+          rules={[{ required: true, message: nameFailMessage }]}
+        >
+          <MyInput />
+        </Form.Item>
+      </>
+    );
+  }
+
+  function getFieldEmail() {
+    return (
+      <>
+        <Text>{emailTitle}</Text>
+        <Form.Item
+          name={emailTitle}
+          rules={[{ required: true, message: emailFailMessage }]}
+        >
+          <MyInput />
+        </Form.Item>
+      </>
+    );
+  }
+
+  function getFieldSubject() {
+    return (
+      <>
+        <Text>{subjectTitle}</Text>
+        <Form.Item
+          name={subjectTitle}
+          rules={[{ required: true, message: SubjectFailMessage }]}
+        >
+          <MySelect style={{ width: "35rem", textAlign: "left" }}>
+            {options.map((op) => (
+              <Option value={op}>{op}</Option>
+            ))}
+          </MySelect>
+        </Form.Item>
+      </>
+    );
+  }
+
+  function getFieldPhone() {
+    return (
+      <>
+        <Text>{phoneTitle}</Text>
+        <Form.Item
+          name={phoneTitle}
+          rules={[{ required: true, message: phoneFailMessage }]}
+        >
+          <MyInput />
+        </Form.Item>
+      </>
+    );
+  }
+
+  function getFieldMessage() {
+    return (
+      <>
+        <Text>{messageTitle}</Text>
+        <Form.Item
+          name={messageTitle}
+          rules={[{ required: true, message: messageFailMessage }]}
+        >
+          <MessageInput rows={messageRows} />
+        </Form.Item>
+      </>
+    );
+  }
+
+  function getSubmitButton() {
+    return (
+      <>
+        <MyButton onClick={onFinish}>{buttonText}</MyButton>
+      </>
+    );
+  }
+
   return (
     <Container>
       <Title>{title}</Title>
       <Form form={form} onFinish={onFinish}>
         <VerticalBox>
           <HorizontalBox>
-            <VerticalBox>
-              <Text>{nameTitle}</Text>
-              <Form.Item
-                name={nameTitle}
-                rules={[{ required: true, message: "Nome é necessário" }]}
-              >
-                <MyInput />
-              </Form.Item>
-            </VerticalBox>
-            <VerticalBox>
-              <Text>{emailTitle}</Text>
-              <Form.Item
-                name={emailTitle}
-                rules={[{ required: true, message: "E-mail é necessário" }]}
-              >
-                <MyInput />
-              </Form.Item>
-            </VerticalBox>
+            <VerticalBox>{getFieldName()}</VerticalBox>
+            <VerticalBox>{getFieldEmail()}</VerticalBox>
           </HorizontalBox>
           <HorizontalBox>
-            <VerticalBox>
-              <Text>{subjectTitle}</Text>
-              <Form.Item
-                name="gender"
-                rules={[{ required: true, message: "Assunto é necessário" }]}
-              >
-                <MySelect style={{ width: "35rem", textAlign: "left" }}>
-                  {options.map((op) => (
-                    <Option value={op}>{op}</Option>
-                  ))}
-                </MySelect>
-              </Form.Item>
-            </VerticalBox>
-            <VerticalBox>
-              <Text>{phoneTitle}</Text>
-              <Form.Item
-                name={phoneTitle}
-                rules={[{ required: true, message: "Telefone é necessário" }]}
-              >
-                <MyInput />
-              </Form.Item>
-            </VerticalBox>
+            <VerticalBox>{getFieldSubject()}</VerticalBox>
+            <VerticalBox>{getFieldPhone()}</VerticalBox>
           </HorizontalBox>
-          <VerticalBox>
-            <Text>{messageTitle}</Text>
-            <MessageInput rows={4}/>
-            <MyButton onClick={onFinish}>{buttonText}</MyButton>
-          </VerticalBox>
+          <VerticalBox>{getFieldMessage()}</VerticalBox>
+          <VerticalBox>{getSubmitButton()}</VerticalBox>
         </VerticalBox>
       </Form>
     </Container>
