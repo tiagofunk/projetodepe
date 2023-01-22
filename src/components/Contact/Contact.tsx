e24c12ebaeba0e1e744ac50cc748c512fa7548d2
Tiago Funk <tiagoff.tf@gmail.com>
2023-01-22 18:38:47 -0300

    Primeira versão do envio do email

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -29,6 +29,7 @@ export const Contact = () => {
   const messageFailMessage = "Não se esqueça da mensagem";
   const messageRows = 4;
   const buttonText = "Enviar Mensagem";
+  const formSubmitHash = "4bf6dc77a30a97a4dcd258f7001abdbc";
 
   const options = [
     "Voluntário",
@@ -49,6 +50,7 @@ export const Contact = () => {
   });
 
   const onFinish = () => {
+    console.log("foi");
     validateValues();
   };
 
@@ -58,21 +60,27 @@ export const Contact = () => {
       subjectError = "",
       phoneError = "",
       messageError = "";
-      console.log(form.getFieldValue(nameTitle));
+    let isValid = true;
+
     if (!validateName(form.getFieldValue(nameTitle))) {
       nameError = nameFailMessage;
+      isValid = false;
     }
     if (!validateEmail(form.getFieldValue(emailTitle))) {
       emailError = emailFailMessage;
+      isValid = false;
     }
     if (!validateSubject(form.getFieldValue(subjectTitle))) {
       subjectError = subjectFailMessage;
+      isValid = false;
     }
     if (!validatePhone(form.getFieldValue(phoneTitle))) {
       phoneError = phoneFailMessage;
+      isValid = false;
     }
     if (!validateMessage(form.getFieldValue(messageTitle))) {
       messageError = messageFailMessage;
+      isValid = false;
     }
     setErrors({
       ...errors,
@@ -82,29 +90,31 @@ export const Contact = () => {
       phone: phoneError,
       message: messageError,
     });
-    return errors;
+    if (isValid) {
+      form.submit();
+      console.log("Enviou");
+    }
   }
 
   function validateName(name: any) {
-    if( name === undefined ) return false;
+    if (name === undefined) return false;
     let nameRegex = new RegExp("^[0-9a-zA-Z ]+$");
     return nameRegex.test(name);
   }
 
   function validateEmail(email: any) {
-    if( email === undefined ) return false;
+    if (email === undefined) return false;
     let emailRegex = new RegExp("^[0-9a-zA-Z.]+@[0-9a-zA-Z]+\\.[a-zA-Z]{0,3}$");
     return emailRegex.test(email);
   }
 
   function validateSubject(subject: any) {
-    if( subject === undefined ) return false;
-    if( subject === undefined ) return false;
+    if (subject === undefined) return false;
     return true;
   }
 
   function validatePhone(phone: any) {
-    if( phone === undefined ) return false;
+    if (phone === undefined) return false;
     let phoneRegex = new RegExp(
       "^\\(?[1-9]{2}\\)? ?(?:[2-8]|9[1-9])[0-9]{3}\\-?[0-9]{4}$"
     );
@@ -112,8 +122,8 @@ export const Contact = () => {
   }
 
   function validateMessage(message: any) {
-    if( message === undefined ) return false;
-    if( message.length === 0 ) return false;
+    if (message === undefined) return false;
+    if (message.length === 0) return false;
     return true;
   }
 
@@ -184,7 +194,7 @@ export const Contact = () => {
   function getSubmitButton() {
     return (
       <>
-        <MyButton onClick={onFinish}>{buttonText}</MyButton>
+        <MyButton htmlType="submit" onClick={onFinish}>{buttonText}</MyButton>
       </>
     );
   }
@@ -192,17 +202,22 @@ export const Contact = () => {
   return (
     <Container>
       <Title>{title}</Title>
-      <Form form={form} onFinish={onFinish}>
+      <Form
+        form={form}
+        target="_blank"
+        action="https://formsubmit.co/rsramalhovf@gmail.com"
+        method="POST"
+      >
         <VerticalBox>
           <HorizontalBox>
             <VerticalBox>{getFieldName()}</VerticalBox>
-            <VerticalBox>{getFieldEmail()}</VerticalBox>
+            {/* <VerticalBox>{getFieldEmail()}</VerticalBox> */}
           </HorizontalBox>
           <HorizontalBox>
-            <VerticalBox>{getFieldSubject()}</VerticalBox>
-            <VerticalBox>{getFieldPhone()}</VerticalBox>
+            {/* <VerticalBox>{getFieldSubject()}</VerticalBox>
+            <VerticalBox>{getFieldPhone()}</VerticalBox> */}
           </HorizontalBox>
-          <VerticalBox>{getFieldMessage()}</VerticalBox>
+          {/* <VerticalBox>{getFieldMessage()}</VerticalBox> */}
           <VerticalBox>{getSubmitButton()}</VerticalBox>
         </VerticalBox>
       </Form>
